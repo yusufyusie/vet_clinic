bd1e590417cda8c2228475c387f715a019325f9c
yusufyusie <yesuf023@gmail.com>
2023-10-06 20:08:13 +0300

    Add Create a table named animals

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
-	('Agumon', '2020,02,03', 0, TRUE, 10.23),
-	('Gabumon', '2018-11-15', 2, TRUE, 8),
-	('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-	('Devimon', '2017-05-12', 5, TRUE, 11);
+	(1, 'Agumon', '2020,02,03', 0, TRUE, 10.23),
+	(2, 'Gabumon', '2018-11-15', 2, TRUE, 8),
+	(3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04),
+	(4, 'Devimon', '2017-05-12', 5, TRUE, 11);
--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
--- Find all animals whose name ends in "mon".
 
 SELECT * from animals WHERE name LIKE '%mon';
 
