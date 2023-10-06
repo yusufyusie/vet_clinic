c4bb95d67a4f61f270ac81537ddbf94aaa2301d0
yusufyusie <yesuf023@gmail.com>
2023-10-06 23:35:42 +0300

    Add updation

--- queries.sql
+++ queries.sql
@@ -33,3 +33,47 @@ SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 -----------------------------------------------
 -----------------------------------------------
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'unspecified'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+
+DELETE from animals;
+
+ROLLBACK;
+
+BEGIN;
+
+DELETE from animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK to sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
+-----------------------------------------------
+-----------------------------------------------
