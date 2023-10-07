e2311a3ae54f38bff374b9f9115e3dee34777484
yusufyusie <yesuf023@gmail.com>
2023-10-07 11:52:21 +0300

    Write queries (using JOIN)

--- data.sql
+++ data.sql
@@ -33,6 +33,8 @@ VALUES ('Ditto', '2022-05-14', 4, true, 22);
 -------------------------------------------------
 -------------------------------------------------
 
+-- Insert the following data into the owners table
+
 INSERT INTO owners (full_name, age) VALUES 
 ('Sam Smith', 34),
 ('Jennifer Orwell', 19),
@@ -41,6 +43,25 @@ INSERT INTO owners (full_name, age) VALUES
 ('Dean Winchester', 14),
 ('Jodie Whittaker', 38);
 
+-- Insert the following data into the species table
+
 INSERT INTO species (name) VALUES 
 ('Pokemon'),
 ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+
+UPDATE animals
+SET species_id = 
+  CASE 
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+  END;
+
+--   Modify your inserted animals to include owner information (owner_id)
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
