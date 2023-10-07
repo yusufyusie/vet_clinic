5fb31f789eb1c46cd9398bf7b9d9da4627031191
yusufyusie <yesuf023@gmail.com>
2023-10-07 10:48:01 +0300

    Modify animals table

--- schema.sql
+++ schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE animals (
 
 -----------------------------------------------
 -----------------------------------------------
--- Add utoincreament of id
+-- Add autoincremented PRIMARY KEY of id
 ALTER TABLE animals
 ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 -- Add decimal places to two
@@ -22,10 +22,27 @@ ALTER COLUMN weight_kg TYPE DECIMAL(10,2);
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
 
-
+-- Create a table named owners with the following columns
 
 CREATE TABLE owners (
   id BIGSERIAL PRIMARY KEY,
   full_name VARCHAR(255),
   age INTEGER
 );
+
+-- Create a table named species with the following columns
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- Modify animals table [Make sure that id is set as autoincremented PRIMARY KEY]
+
+ALTER TABLE animals 
+DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
